092d4fa339eab393265b4db67b43a184dede8975
Bhuvanteja <tejabtr753@gmail.com>
2022-03-05 17:19:40 +0530

    removed picker

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import logo from "./assets/LogoName.png"
 
-import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
 
@@ -13,19 +12,6 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Image source={logo} style={styles.logo} />
       <Text style={styles.instructions} >Choose You Language</Text>
-      <Picker
-        selectedValue={country}
-        onValueChange={(value, index) => setCountry(value)}
-        mode="dropdown" // Android only
-        style={styles.picker}
-      >
-        <Picker.Item label="Please select your country" value="Unknown" />
-        <Picker.Item label="Australia" value="Australia" />
-        <Picker.Item label="Belgium" value="Belgium" />
-        <Picker.Item label="Canada" value="Canada" />
-        <Picker.Item label="India" value="India" />
-        <Picker.Item label="Japan" value="Japan" />
-      </Picker>
       <TouchableOpacity onPress={() => alert('Page Coming soon!')} style={styles.button}>
         <Text style={styles.buttonText}>PROCEED</Text>
       </TouchableOpacity>
@@ -64,12 +50,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     color: '#fff',
-  }, 
-  picker: {
-    marginVertical: 30,
-    width: 300,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: "#666",
   },
 });
